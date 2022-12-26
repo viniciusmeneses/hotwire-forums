6c1f9f4f2861ebce9d39cc5ee83d2fb8ca80605f
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-26 00:44:04 -0300

    feat: create discussions from the UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
 
-  resources :discussions, only: [:index]
+  resources :discussions, only: [:index, :new, :create]
 end

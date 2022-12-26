73e3611928623f25d8c8564b04dc868a8dc5a636
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-26 18:10:53 -0300

    feat: add new post on discussion show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
 
-  resources :discussions
+  resources :discussions do
+    resources :posts, only: [:create], module: :discussions
+  end
 end

1ee2db5ad22192affb0c610e5d4ac56dd170f2b9
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-26 16:13:13 -0300

    feat: setup post model and add posts_count to discussions

---
+++ db/migrate/20221226190502_add_posts_count_to_discussions.rb
@@ -0,0 +1,5 @@
+class AddPostsCountToDiscussions < ActiveRecord::Migration[7.0]
+  def change
+    add_column :discussions, :posts_count, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_26_185631) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_26_190502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +59,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_185631) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count", default: 0
     t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "discussion_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["discussion_id"], name: "index_posts_on_discussion_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_185631) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "discussions", "users"
+  add_foreign_key "posts", "discussions"
+  add_foreign_key "posts", "users"
 end

d631aaf7c207c4e719f307726cfb328b286cc330
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-26 00:57:53 -0300

    feat: remove discussion

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
 
-  resources :discussions, only: [:index, :new, :create, :edit, :update]
+  resources :discussions, only: [:index, :new, :create, :edit, :update, :destroy]
 end

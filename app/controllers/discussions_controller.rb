e0456daa56defa5d0212189486a1fe3945bc7045
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-27 13:21:20 -0300

    feat: add realtime moving of categories

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -35,6 +35,20 @@ class DiscussionsController < ApplicationController
     respond_to do |format|
       if @discussion.update(discussion_params)
         @discussion.broadcast_replace(partial: "discussions/header", locals: { discussion: @discussion })
+
+        if @discussion.saved_change_to_category_id?
+          old_category_id, new_category_id = @discussions.saved_change_to_category_id
+
+          old_category = Category.find(old_category_id)
+          new_category = Category.find(new_category_id)
+
+          @discussion.broadcast_remove_to(old_category)
+          @discussion.broadcast_prepend_to(new_category)
+
+          old_category.reload.broadcast_replace_to("categories")
+          new_category.reload.broadcast_replace_to("categories")
+        end
+
         format.html { redirect_to @discussion, notice: "Discussion updated" }
       else
         format.html { render :edit, status: :unprocessable_entity }

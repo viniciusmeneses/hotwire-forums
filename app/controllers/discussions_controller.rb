23cd1be28e0a6deaa405ed3c377c741af358aab0
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-27 11:36:47 -0300

    feat: add categories to the app

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -50,7 +50,7 @@ class DiscussionsController < ApplicationController
   private
 
   def discussion_params
-    params.require(:discussion).permit(:name, :closed, :pinned, posts_attributes: :body)
+    params.require(:discussion).permit(:name, :category_id, :closed, :pinned, posts_attributes: :body)
   end
 
   def set_discussion
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_26_190502) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_142928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_190502) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "discussions_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "discussions", force: :cascade do |t|
     t.string "name"
     t.boolean "pinned", default: false
@@ -60,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_26_190502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "posts_count", default: 0
+    t.bigint "category_id"
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 

a952e9f1474c67975aa1721002a8c3760ad5163a
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-29 14:32:41 -0300

    feat: update discussion model to have methods to toggle subscription

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,7 +1,16 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   belongs_to :category, counter_cache: true, touch: true
+
   has_many :posts, dependent: :destroy
+  has_many :users, through: :posts
+  has_many :discussion_subscriptions, dependent: :destroy
+  has_many :optin_subscribers, -> { where(discussion_subscriptions: { subscription_type: :optin }) },
+    through: :discussion_subscriptions,
+    source: :user
+  has_many :optout_subscribers, -> { where(discussion_subscriptions: { subscription_type: :optout }) },
+    through: :discussion_subscriptions,
+    source: :user
 
   # discussion.category_name
   delegate :name, prefix: :category, to: :category, allow_nil: true
@@ -21,5 +30,25 @@ class Discussion < ApplicationRecord
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+  end
+
+  def subscribed_users
+    (users + optin_subscribers).uniq - optout_subscribers
+  end
+
+  def subscription_for(user)
+    return if user.nil?
+
+    discussion_subscriptions.find_by(user_id: user.id)
+  end
+
+  def toggle_subscription(user)
+    if (subscription = subscription_for(user))
+      subscription.toggle!
+    elsif posts.where(user_id: user.id).any?
+      discussion_subscriptions.create(user: user, subscription_type: "optout")
+    else
+      discussion_subscriptions.create(user: user, subscription_type: "optin")
+    end
   end
 end
--- app/models/discussion_subscription.rb
+++ app/models/discussion_subscription.rb
@@ -5,15 +5,15 @@ class DiscussionSubscription < ApplicationRecord
   scope :optin, -> { where(subscription_type: :optin) }
   scope :optout, -> { where(subscription_type: :optout) }
 
-  validates :subscription_type, presence: true, inclusion: { in: %w[optin, optout] }
+  validates :subscription_type, presence: true, inclusion: { in: %w[optin optout] }
   validates :user_id, uniqueness: { scope: :discussion_id }
 
   def toggle!
     case subscription_type
     when "optin"
-      update(subscription_type: "optout")
+      update!(subscription_type: "optout")
     when "optout"
-      update(subscription_type: "optin")
+      update!(subscription_type: "optin")
     end
   end
 end

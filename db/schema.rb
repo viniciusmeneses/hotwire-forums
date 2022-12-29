0b159163086b7cad17bff8e7b1fb087323a5623a
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-29 10:50:12 -0300

    feat: start on discussion subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_142928) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_29_134509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_142928) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "discussion_subscriptions", force: :cascade do |t|
+    t.bigint "discussion_id", null: false
+    t.bigint "user_id", null: false
+    t.string "subscription_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["discussion_id"], name: "index_discussion_subscriptions_on_discussion_id"
+    t.index ["user_id"], name: "index_discussion_subscriptions_on_user_id"
+  end
+
   create_table "discussions", force: :cascade do |t|
     t.string "name"
     t.boolean "pinned", default: false
@@ -97,6 +107,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_142928) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "discussion_subscriptions", "discussions"
+  add_foreign_key "discussion_subscriptions", "users"
   add_foreign_key "discussions", "users"
   add_foreign_key "posts", "discussions"
   add_foreign_key "posts", "users"

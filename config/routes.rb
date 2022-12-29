818518df26c520e5c61bc2017dd02fde5cc28a70
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-12-29 15:19:58 -0300

    feat: display notifications in UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,24 @@ Rails.application.routes.draw do
 
   resources :discussions do
     resources :posts, only: %i[create show edit update destroy], module: :discussions
-    resources :notifications, only: %i[create], module: :discussions
+    resources :notifications, only: :create, module: :discussions
 
     collection do
-      get 'categories/:id', to: 'categories/discussions#index', as: :category
+      get "categories/:id", to: "categories/discussions#index", as: :category
+    end
+  end
+
+  resources :notifications, only: :index do
+    collection do
+      post "mark_as_read", to: "notifications#read_all", as: :read
     end
   end
 
   unauthenticated do
-    root to: 'main#index', as: :unauthenticated_root
+    root to: "main#index", as: :unauthenticated_root
   end
 
   authenticated do
-    root to: 'discussions#index', as: :authenticated_root
+    root to: "discussions#index", as: :authenticated_root
   end
 end
